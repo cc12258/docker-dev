1dac26e10d17ece12127496569cfb946a005c9c8
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-06-14 21:10:50 +0000

    Update branch for image nodejs-dev

---
+++ base/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+set -x
+
+cat /tmp/plugin.vim >> ~/.config/nvim/plugin.vim
+sudo rm /tmp/plugin.vim
+
+cat /tmp/post-plugin.vim >> ~/.config/nvim/post-plugin.vim
+sudo rm /tmp/post-plugin.vim
+
+# Install node version manager
+curl -o- https://raw.githubusercontent.com/AGhost-7/nvm/master/install.sh | bash
+
+# customize fzf to ignore node_modules
+cat /tmp/bashrc-additions.sh >> ~/.bashrc
+sudo rm /tmp/bashrc-additions.sh
+
+# jq, the greatest command line JSON querier
+sudo apt-get update
+sudo apt-get install jq -y
+
+ycm-install
+
+# Cleanup whats left...
+sudo apt-get autoremove -y
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
+# Install vim plugins
+nvim +PlugInstall +qall
+
---
+++ base/post-plugin.vim
@@ -0,0 +1,5 @@
+" I don't want search results from node_modules in the fuzzy searcher
+let g:ctrlp_custom_ignore = {
+	\ 'dir': '\v[\/](node_modules|\.git)$'
+	\ }
+
