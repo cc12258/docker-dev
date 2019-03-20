74175506eef740013486a5a2ef43782175ccb2ed
Chris Cameron <chriscamero@me.com>
2019-03-20 13:33:05 -0600

    Add Terraform and chef dk

--- images/ruby-dev/build.sh
+++ images/ruby-dev/build.sh
@@ -17,6 +17,20 @@ BASHRC
 
 sudo apt-get update
 
+# Install terraform
+TERRAFORM_VERSION=0.11.13
+TERRAFORM_SHA256SUM=5925cd4d81e7d8f42a0054df2aafd66e2ab7408dbed2bd748f0022cfe592f8d2
+sudo apt-get install -y curl
+curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip > terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
+    echo "${TERRAFORM_SHA256SUM}  terraform_${TERRAFORM_VERSION}_linux_amd64.zip" > terraform_${TERRAFORM_VERSION}_SHA256SUMS && \
+    sudo sha256sum -c --strict terraform_${TERRAFORM_VERSION}_SHA256SUMS && \
+    sudo unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /bin && \
+    sudo rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Install chef development kit
+wget https://packages.chef.io/files/stable/chefdk/3.2.30/ubuntu/18.04/chefdk_3.2.30-1_amd64.deb
+dpkg -i chefdk_3.2.30-1_amd64.deb
+
 # base ruby distribution
 sudo apt-get install -y ruby ruby-dev
 
